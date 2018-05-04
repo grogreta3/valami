bbef9e96dc514477a1b3ed323811e920fbe08704
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2018-05-04 08:42:33 +0200

    Add files via upload

---
+++ 5_feladat.py
@@ -0,0 +1,13 @@
+def feladat_5():
+    sum = 0
+    while sum != 100:
+        x = int(input("Szam:"))
+        if sum < 100:
+            sum += x
+        if sum > 100:
+            sum -= x
+
+    return sum
+
+
+print(feladat_5())
