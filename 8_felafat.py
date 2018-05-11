23df80f9d0979e11478dd6c1a498d8a8a256fe20
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2018-05-11 14:01:22 +0200

    Update 8_felafat.py

--- 8_felafat.py
+++ 8_felafat.py
@@ -1,23 +1,3 @@
-'''
-first = input("Adja meg az első szót! ")
-second = input("Adja meg a második szót! ")
-
-
-
-kozos=[]
-for i in range(0,len(first)):
-    for j in range(0,len(second)):
-        if first[i] == second[j] :
-            kozos.append(second[j])
-
-vegso=""
-for i in kozos:
-    if i not in vegso:
-        vegso+=i
-
-print(vegso)
-'''
-
 def Feladat8():
     a = input('Első szó:')      #Bekéri az első szót
     b = input('Második szó:')   #Bekéri a másodikat
