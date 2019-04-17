f857301aa60090a5b0fc4822e4049426d1058f11
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2019-04-17 15:10:46 +0200

    5. feladat

--- 5. feladat.py
+++ 5. feladat.py
@@ -1,5 +1,23 @@
-def valami():
-    n=1
-    if n>0:
-        return 0
+def lista(li):
+    uj_li=[]
+    i=0
+    while i!=len(li)-1:
+        if li[i]<li[i+1]:
+            uj_li.append(li[i])
+            i+=1
+        if li[i]>li[i+1]:
+            break
+    if li[i]>li[i-1]:
+        uj_li.append(li[i])
+
+    return uj_li,len(uj_li)
+
+li=input('adj meg egy listát vesszővel elválasztva:')
+li=li.split(',')
+uj_li=[]
+for i in li:
+    i=int(i)
+    uj_li.append(i)
+print(uj_li)
+print(lista(uj_li))
 
