916c22fa43057794397c03fc1613fbe7a22d5b28
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2019-04-24 19:29:12 +0200

    5. feladat

---
+++ 31.feladat.py
@@ -0,0 +1,15 @@
+#Íef emailolvaso(n):
+
+
+
+
+n=input("adj meg egy email cimet:")
+while True:
+
+
+
+
+#email=input("adj meg egy email cimet:")
+#email1=email.split("@")
+#email2=email.split(".")
+#print(email1,email2)
