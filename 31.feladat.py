426b33a0d7143f838418ebf079dc7185466e8131
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2019-05-13 18:47:20 +0200

    5. feladat

--- 31.feladat.py
+++ 31.feladat.py
@@ -1,23 +1,11 @@
-#Íef emailolvaso(n):
-
 n = input("adj meg egy email cimet:")
 while n!="0":
-    if "@" and "." not in n:
-        print("nem megfelelo az email cim")
-        n = input("adj meg egy email cimet:")
-    elif "@" and "."  in n:
+    if "@" and "."  in n:
         n=n.split("@")
         if type(n[0][0])!=str and type(n[0][0])!=int:
             print("nem betuvel vagy szammal kezdtel")
             n = input("adj meg egy email cimet:")
-    else:
-        print(True)
+    if "@" or "." not in n:
+        print("nem jo az email cim")
         n = input("adj meg egy email cimet:")
 
-
-
-
-#email=input("adj meg egy email cimet:")
-#email1=email.split("@")
-#email2=email.split(".")
-#print(email1,email2)
