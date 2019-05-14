23a0e992345328e59b6fa373741898ec40df391b
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2019-05-14 20:13:23 +0200

    5. feladat

--- 31.feladat.py
+++ 31.feladat.py
@@ -1,14 +1,15 @@
 n = input("adj meg egy email cimet:")
 while n!="0":
-    if "@" and "."  in n:
-        n=n.split("@")
-        if type(n[0][0])!=str and type(n[0][0])!=int:
-            print("nem betuvel vagy szammal kezdtel")
+    for i in n:
+        if "@" and "."  in n:
+            n=n.split("@")
+            if type(n[0][0])!=str and type(n[0][0])!=int:
+                print("nem betuvel vagy szammal kezdtel")
+                n = input("adj meg egy email cimet:")
+        if "@" or "." not in n:
+            print("nem jo az email cim")
             n = input("adj meg egy email cimet:")
-    if "@" or "." not in n:
-        print("nem jo az email cim")
-        n = input("adj meg egy email cimet:")
 
-    else:
-        print(True)
-        n = input("adj meg egy email cimet:")
+        else:
+            print(True)
+            n = input("adj meg egy email cimet:")
--- 5. feladat.py
+++ 5. feladat.py
@@ -1,3 +1,4 @@
+"""
 li=input('adj meg egy szam sort vesszovel elvalasztva:')
 li=li.split(",")
 new_li=[]
@@ -12,16 +13,27 @@ if li[-1]>new_li[-1]:
     new_li.append(li[idx])
 
 print(new_li)
+"""
 
+li=input('adj meg egy szam sort vesszovel elvalasztva:')
+li=li.split(',')
+new_li=[li[0]]
+for i in range(len(li)-1):
+    i=int(i)
+    if li[i]<li[i+1]:
+        new_li.append(li[i+1])
+    else:
+        newli2=[li[0]]
+        for j in range (len(newli2)-1):
+            j=int(j)
+            if li[i]>newli2[j]:
+                newli2.append(li[i])
 
+        if len(new_li)<len(newli2):
+            new_li=newli2
+            newli2=[]
+            print(newli2)
+print(new_li)
 
-
-   # elif li[i]>li[i+1] and li[i]>li[0]:
-    #    new_li2=[li[0]]
-        
-   #     if len(new_li)>len(new_li2):
-   #         print(new_li)
-    #    else:
-    #        print(nem_li2)
 
 
