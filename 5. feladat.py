7b8ad2eaa9fbfa682f1bf48ecae42999f9d93cf1
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2019-05-16 01:20:42 +0200

    Update 5. feladat.py

--- 5. feladat.py
+++ 5. feladat.py
@@ -1,21 +1,25 @@
-
 li=input('adj meg egy szam sort vesszovel elvalasztva:')
 li=li.split(',')
-new_li=[li[0]]
-for i in range(len(li)-1):
+new_li=[]
+new_li2=[]
+n=len(li)-1
+for i in range(n):
     if li[i]<li[i+1]:
         new_li.append(li[i])
-    elif li[i]>li[i+1]:
+        print(new_li,new_li2)
+    if li[i]>li[i+1]:
         new_li.append(li[i])
-        if len(new_li)>len(new_li2)
-            new_li2=nee_li
-            new_li=[li[i]]
+        if len(new_li)>len(new_li2):
+            new_li2=new_li
+            new_li=[li[0]]
+            print(new_li,new_li2)
         else:
-            new_li=[li[i]]
+            new_li=[li[0]]
+
 if li[-1]>new_li[-1]:
     new_li.append(li[-1])
-
-print(new_li)
-
-
+if len(new_li2)==0:
+    print(new_li,len(new_li))
+else:
+    print(new_li2, len(new_li2))
 
